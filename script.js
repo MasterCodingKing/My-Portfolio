ee913f56cc76810dbbbbe0a93f5fca8a563605e5
chano <chano@gmail.com>
2023-08-19 17:53:50 +0800

    done contacts

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+   let list_item_1 = document.querySelector('.list-title-1')
+   let list_item_2 = document.querySelector('.list-title-2')
+   let list_item_3 = document.querySelector('.list-title-3')
+
+   
+   let list_group_1 = document.querySelector('.list-group-1')
+   let list_group_2 = document.querySelector('.list-group-2')
+   let list_group_3 = document.querySelector('.list-group-3')
+
+   list_item_1.addEventListener('mouseover', ()=>{
+        list_group_1.style.display = "block";
+        
+   })
+   list_item_1.addEventListener('mouseout', ()=>{
+    list_group_1.style.display = "none";
+   })
+   list_item_2.addEventListener('mouseover', ()=>{
+    list_group_2.style.display = "block"
+   })
+   list_item_2.addEventListener('mouseout', ()=>{
+    list_group_2.style.display = "none"
+   })
+   list_item_3.addEventListener('mouseover', ()=>{
+    list_group_3.style.display = "block"
+   })
+   list_item_3.addEventListener('mouseout', ()=>{
+    list_group_3.style.display = "none"
+   })
+
+});
